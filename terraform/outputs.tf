c0840560a1d69af6801192de9b27983e1d0489f4
Tanishka33 <tanishka.3315@gmail.com>
2026-06-07 09:35:46 +0530

    terraform formatting

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "cluster_name" {
-    value = aws_eks_cluster.voting.name
+  value = aws_eks_cluster.voting.name
 }
 
 output "cluster_endpoint" {
-    value = aws_eks_cluster.voting.endpoint
+  value = aws_eks_cluster.voting.endpoint
 }
